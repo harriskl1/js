a8b4af98285549afedb674cff664d7e83ff8bb74
harriskl1 <38195817+harriskl1@users.noreply.github.com>
2018-09-25 20:39:09 -0400

    Add files via upload

--- Instructions/StarterCode/static/js/app.js
+++ Instructions/StarterCode/static/js/app.js
@@ -1,46 +1,55 @@
 // from data.js
 var tableData = data;
-
-
 var tbody = d3.select("tbody");
-
 // Console.log the weather data from data.js
  console.log(tableData);
 
  function buildTable(data){
+    // First, clear out any existing data
+    tbody.html("");
     data.forEach(function(dataRow) {
-    console.log(dataRow);
-    var row = tbody.append("tr");
-
-    Object.entries(dataRow).forEach(([key, value]) => {
-        var cell = tbody.append("td");
-        cell.text(value);
-      });
-    });
-};
-// function clickbutton(){
-//      // Prevent the page from refreshing
-//   d3.event.preventDefault();
-//  var date = d3.select("#datetime").property("value");
+        
+        console.log(dataRow);
+        var row = tbody.append("tr");
+        Object.entries(dataRow).forEach(([key, value]) => {
+            var cell = tbody.append("td");
+            cell.text(value);
+   });
+  });
  
- buildTable(tableData);
+}
 
- var inputElement = d3.select("#filter-btn");
+function clickButton(){
+//   // Prevent the page from refreshing
+//  d3.event.preventDefault();
+// var date = d3.select("#datetime").property("value");
 
-inputElement.on("click", function() {
 
-  // Prevent the page from refreshing
-  d3.event.preventDefault();
+
+ // Prevent the page from refreshing
+ d3.event.preventDefault();
  
-  var inputE = d3.select("#datetime");
-  console.log(inputE);
-
+ var inputE = d3.select("#datetime");
+ console.log(inputE);
  // Get the value property of the input element
-  var inputValue = inputE.property("value");
+ var inputValue = inputE.property("value");
+ console.log(inputValue);
+ console.log(tableData);
 
-  console.log(inputValue);
-  console.log(tableData);
 
-  var filteredData = tableData.filter(chart => chart.datetime === inputValue);
-  console.log(filteredData)
-});
+ let filteredData = tableData;
+
+if (inputValue){
+    filteredData = filteredData.filter(row => row.datetime === inputValue);
+    console.log(filteredData)
+}
+
+ buildTable(filteredData);
+}
+// var inputElement = d3.select("#filter-btn");
+// use d3 to selec the "#filter-btn"
+
+d3.selectAll("#filter-btn").on("click", clickButton);
+// inputElement.on("click", clickButton); 
+
+buildTable(tableData);
